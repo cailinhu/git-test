38604e0b1c6f5b019b5fe4d821368ca27b7d9f52
cailinhu <785001593@qq.com>
2022-11-15 11:24:43 +0800

    first commit

---
+++ src/main/java/com/test/GitTestJava.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @Author cailinhu
+ * @Description TODO
+ * @Date 2022/11/15 11:21
+ * @Version 1.0
+ */
+public class GitTestJava {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
