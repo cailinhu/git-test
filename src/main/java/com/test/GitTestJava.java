040aa4d39a1ea224a1425203355a19c1e44c1a00
cailinhu <785001593@qq.com>
2022-11-15 11:35:52 +0800

    hot-fix commit

--- src/main/java/com/test/GitTestJava.java
+++ src/main/java/com/test/GitTestJava.java
@@ -10,5 +10,6 @@ public class GitTestJava {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello hot-fix!");
     }
 }
